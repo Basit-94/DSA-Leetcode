09d673b22200d71c0e4c72540ec693fd2a27c0c0
Abdul Basit Siddiqui <abdulsiddiqui0605@gmail.com>
2026-09-11 20:54:49 +0530

    Time: 2 ms (73.02%) | Memory: 43 MB (12.75%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int left=0,right=0,count=0;
+        if(t.length()<s.length())
+        {
+            return false;
+        }
+        if(s.length()==0)
+        {
+            return true;
+        }
+        while(left<s.length() && right<t.length())
+        {
+            char c1 = s.charAt(left);
+            char c2 = t.charAt(right);
+
+            if(c1==c2)
+            {
+                count++;
+                left++;
+            }
+            right++;
+        }
+
+        if(count==s.length())
+        {
+            return true;
+        }
+        return false;
+    }
+}
